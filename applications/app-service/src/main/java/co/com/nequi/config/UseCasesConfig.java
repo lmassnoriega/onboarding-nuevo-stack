7e449574a7e38525094910348d55e4c137786dee
Luis Mass Noriega <massl@uninorte.edu.co>
2023-02-08 22:43:10 -0500

    feat: Repo init Modified gitignore to avoid .idea folder store.

---
+++ applications/app-service/src/main/java/co/com/nequi/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package co.com.nequi.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "co.com.nequi.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
