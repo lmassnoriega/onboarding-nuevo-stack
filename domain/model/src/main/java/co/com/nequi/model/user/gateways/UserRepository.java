20d0a4829e297b2100be88eabbf56a2e022d7da9
Luis Mass Noriega <massl@uninorte.edu.co>
2023-02-09 23:30:26 -0500

    chore: Added Java docs for gateways and use cases.

--- domain/model/src/main/java/co/com/nequi/model/user/gateways/UserRepository.java
+++ domain/model/src/main/java/co/com/nequi/model/user/gateways/UserRepository.java
@@ -6,8 +6,37 @@ import reactor.core.publisher.Mono;
 
 public interface UserRepository {
 
+    /**
+     * Obtains a user from local storage using the provided identifier.
+     *
+     * @param id
+     *         Identifier belonging to the user being retrieved.
+     * @return {@link User} User from local storage
+     */
     Mono<User> getById(Long id);
+
+    /**
+     * Saves a user to local storage
+     *
+     * @param userToSave
+     *         User request body information to be persisted.
+     * @return A {@link Mono} of the user just persisted to local storage
+     */
     Mono<User> saveUser(User userToSave);
+
+    /**
+     * Returns all available {@link User} from local storage
+     *
+     * @return A {@link Flux} of users from local storage.
+     */
     Flux<User> getAllUsers();
+
+    /**
+     * Queries all users given the value of name
+     *
+     * @param name
+     *         param name to look for user in the database.
+     * @return A {@link Flux} of users from local storage.
+     */
     Flux<User> getByName(String name);
 }
--- domain/usecase/src/main/java/co/com/nequi/usecase/user/UserUseCase.java
+++ domain/usecase/src/main/java/co/com/nequi/usecase/user/UserUseCase.java
@@ -14,10 +14,25 @@ public class UserUseCase {
     private final UserRepository userDBRepository;
     private final UserExternalRepository userExternalRepository;
 
+    /**
+     * Saves a user to local storage
+     *
+     * @param userToSave
+     *         User request body information to be persisted.
+     * @return A {@link Mono} of the user just persisted to local storage
+     */
     public Mono<User> saveDBUser(User userToSave) {
         return userDBRepository.saveUser(userToSave);
     }
 
+    /**
+     * Obtains a user from external source using the provided identifier and
+     * then saves it to local storage.
+     *
+     * @param id
+     *         Identifier belonging to the user being retrieved.
+     * @return {@link User} User from external storage
+     */
     public Mono<User> saveExternalUser(Long id) {
         return userDBRepository.getById(id).flatMap(user -> Mono.error(
                 new BusinessException("User already exists"))).switchIfEmpty(
@@ -25,15 +40,34 @@ public class UserUseCase {
                         .flatMap(userDBRepository::saveUser))).cast(User.class);
     }
 
+    /**
+     * Obtains a user from local storage using the provided identifier.
+     *
+     * @param id
+     *         Identifier belonging to the user being retrieved.
+     * @return {@link Mono} User from local storage
+     */
     public Mono<User> getUserById(Long id) {
-        return userDBRepository.getById(id)
-                .switchIfEmpty(Mono.error(new BusinessException("User not found")));
+        return userDBRepository.getById(id).switchIfEmpty(
+                Mono.error(new BusinessException("User not found")));
     }
 
+    /**
+     * Returns all available {@link User} from local storage
+     *
+     * @return A {@link Flux} of users from local storage.
+     */
     public Flux<User> getAllUsers() {
         return userDBRepository.getAllUsers();
     }
 
+    /**
+     * Queries all users given the value of name
+     *
+     * @param nameToLookup
+     *         param name to look for user in the database.
+     * @return A {@link Flux} of users from local storage.
+     */
     public Flux<User> getAllByName(String nameToLookup) {
         return userDBRepository.getByName(nameToLookup);
     }
