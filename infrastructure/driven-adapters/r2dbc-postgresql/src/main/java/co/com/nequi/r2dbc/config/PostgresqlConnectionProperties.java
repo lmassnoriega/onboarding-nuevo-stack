25c87c2907962a94b9a774a5d504e3617f5e572c
Luis Mass Noriega <massl@uninorte.edu.co>
2023-02-10 01:55:24 -0500

    feat: Added entity mappers. Added reactive repository for custom operations. Added Conection pool properties settings.

--- infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/nequi/r2dbc/config/PostgresqlConnectionProperties.java
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/nequi/r2dbc/config/PostgresqlConnectionProperties.java
@@ -3,10 +3,14 @@ package co.com.nequi.r2dbc.config;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Configuration
+@ConfigurationProperties(prefix = "infrastructure.r2dbc")
 public class PostgresqlConnectionProperties {
 
     private String database;
