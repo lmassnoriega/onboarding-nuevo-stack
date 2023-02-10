1b63167df7e186cd8b4399037565cb1965482e7a
Luis Mass Noriega <massl@uninorte.edu.co>
2023-02-10 02:34:43 -0500

    feat: Added response Model

---
+++ infrastructure/driven-adapters/rest-consumer/src/main/java/co/com/nequi/consumer/response/SupportType.java
@@ -0,0 +1,13 @@
+package co.com.nequi.consumer.response;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SupportType {
+    private String url;
+    private String text;
+}
