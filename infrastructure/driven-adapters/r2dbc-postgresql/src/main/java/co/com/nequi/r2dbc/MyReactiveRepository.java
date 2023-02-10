5b185b08080ad0077f83b6f70a0c4359d4c3ee89
Luis Mass Noriega <massl@uninorte.edu.co>
2023-02-10 01:51:38 -0500

    feat: Added persistence entity Purged given driven adapter files. Added dependencies for spring boot validation.

--- infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/nequi/r2dbc/MyReactiveRepository.java
+++
@@ -1,9 +0,0 @@
-package co.com.nequi.r2dbc;
-
-import org.springframework.data.repository.query.ReactiveQueryByExampleExecutor;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-// TODO: This file is just an example, you should delete or modify it
-public interface MyReactiveRepository extends ReactiveCrudRepository<Object, String>, ReactiveQueryByExampleExecutor<Object> {
-
-}
