87fc90e2931419525bf4824c85295d1cba9cf614
Luis Mass Noriega <massl@uninorte.edu.co>
2023-02-10 00:11:58 -0500

    feat: Added reactive postgres driven adapter. TO-DO Modify adapter config.

---
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/test/java/co/com/nequi/r2dbc/PostgreSQLConnectionPoolTest.java
@@ -0,0 +1,15 @@
+package co.com.nequi.r2dbc.config;
+
+import co.com.nequi.r2dbc.config.PostgreSQLConnectionPool;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class PostgreSQLConnectionPoolTest {
+
+    // TODO: change four you own tests
+    @Test
+    void getConnectionConfig() {
+        PostgreSQLConnectionPool postgreSQLConnectionPool= new PostgreSQLConnectionPool();
+        Assertions.assertNotNull(postgreSQLConnectionPool.getConnectionConfig());
+    }
+}
